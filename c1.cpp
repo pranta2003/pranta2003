2998d6fda7280e39c8d46993f71098f785aeedea
Pranta Kumer Pandit <148738032+pranta2003@users.noreply.github.com>
2024-02-19 18:01:42 +0600

    Add files via upload

---
+++ c1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int temp,b,a;
+
+    cout<<"x="<<endl;
+    cin>>a;
+    cin>>b;
+    temp=a;
+    a=b;
+//b=temp;
+
+    cout<<"now x="<<b<<endl;
+
+}
